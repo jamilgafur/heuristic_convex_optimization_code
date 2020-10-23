7083225babae76b1cd200aaebefe5f8a86925c49
Jamilgafur <jgafur@uiowa.edu>
2020-10-23 11:01:13 -0500

    added formatting

--- GSA.py
+++ GSA.py
@@ -11,27 +11,26 @@ class Algorithm:
         self.A = None
         self.b = None
         self.A, self.b = gi(args["k"], args["size"], args["debug"])
-        
+        self.debug = args["debug"] != -1
+
         self.alpha = args["alpha"]
-        self.G = random.uniform(0, 100) 
+        self.G = 100# random.uniform(0, 100) 
         self.max_iter  = args["number_generations"]
         self.pop_size  = args["pop_size"]
         self.dimension = args["size"]
         
         if args["problems"] == 1:
-          self.cost_func = self.square
+          self.cost_func = self.evalutate_quad_opt
         else:
-          self.cost_func = self.square
-        self._current_iter = 0
+          self.cost_func = self.evalutate_quad_opt
         
-        self.converged = False
-        self.converged_counter = 30
-        self.converged_rate = .00001
+        self._current_iter = 0
+        self.best_so_far_iteration = None
         self.best_so_far = None
         self.worse_so_far = None
         self.write_out = ""
         
-        self.locations = np.array([[ random.uniform(0, 10) for i in range(self.dimension) ] for i in range(self.pop_size)])
+        self.locations = np.array([[ random.uniform(0, 100) for i in range(self.dimension) ] for i in range(self.pop_size)])
         
         #np.random.rand(self.pop_size, self.dimension)
 
@@ -48,12 +47,10 @@ class Algorithm:
     # optimization function 1
     def evalutate_quad_opt(self, individual):
         x = np.array(individual, dtype=float)
-        value = 0.5 * np.matmul(np.matmul(x.T, self.A), x) - np.matmul(self.b.T, x)
+        #value = 0.5 * np.matmul(np.matmul(x.T, self.A), x) - np.matmul(self.b.T, x)
+        value = np.linalg.norm(np.matmul(self.A, x) - self.b, 2)
         return value
 
-    def square(self, individual):
-        x = np.array(individual)
-        return sum([i**2 for i in x])
     
     # Evaluate a single x (x_i)
     def evaluate(self, args):
@@ -61,22 +58,26 @@ class Algorithm:
 
     # Generate the cost of all particles
     def gen_cost_matrix(self):
-        print("\tloc: \n{}".format(self.locations))
+        if self.debug :
+            print("\tloc: \n{}".format(self.locations))
         for i, x in enumerate(self.locations):
             self.cost_matrix[i] = self.evaluate(x)
-        print("\tcmx: \n{}".format(self.cost_matrix))
+        if self.debug :
+            print("\tcmx: \n{}".format(self.cost_matrix))
         self.cal_inertia()
 
     def cal_inertia(self):
         best = np.min(self.cost_matrix)
         worst = np.max(self.cost_matrix)
         self.inertia = (self.cost_matrix - worst) / best - worst
-        #print("\tine: \n{}".format(self.inertia))
+        if self.debug :
+            print("\tine: \n{}".format(self.inertia))
         self.cal_mass()
             
     def cal_mass(self):
         self.mass_matrix = self.inertia / np.sum(self.inertia)
-        print("\tmas: \n{}".format(self.mass_matrix))
+        if self.debug :
+            print("\tmas: \n{}".format(self.mass_matrix))
         self.update_grav()
 
 
@@ -96,9 +97,10 @@ class Algorithm:
                 else:
                     f = f + self.G * (dividend / divisor) * (self.locations[j] - self.locations[i])
 
-            self.f[i] = np.random.uniform(0, 1) * f
+            self.f[i] = f 
             
-        print("\tfce: \n{}".format(self.f))
+        if self.debug :
+            print("\tfce: \n{}".format(self.f))
         
         self.cal_accel()
 
@@ -109,16 +111,18 @@ class Algorithm:
 
     def cal_vel(self):
         self.V = (np.random.uniform(0, 1) * self.V) + self.accel
-        print("\tvel: \n{}".format(self.V))
+        if self.debug :
+            print("\tvel: \n{}".format(self.V))
         self.update_locations()
         
     def update_grav(self):
-        self.G = self.G *  (1 - ( self._current_iter / self.max_iter) )
+        self.G = self.G *  (1 - (self._current_iter / self.max_iter) )
         self.cal_force()
         
     def update_locations(self):
         self.locations = self.locations + self.V
-        print("\tnlc: \n{}".format(self.locations))
+        if self.debug :
+            print("\tnlc: \n{}".format(self.locations))
         self.update_best_so_far()
 
     # set for minimization
@@ -128,30 +132,39 @@ class Algorithm:
         
         if self._current_iter > 1:
         
-            print("checking: {} @ {}".format(self.locations[index], self.evaluate(self.locations[index])))
-            print("with: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
+            if self.debug :
+                print("checking: {} @ {}".format(self.locations[index], self.evaluate(self.locations[index])))
+                print("with: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
             
         
         if self.best_so_far is None:
             self.best_so_far = self.locations[index]
-            print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
+            self.best_so_far_iteration = self._current_iter
+            if self.debug :
+                print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
             
         if self.evaluate(self.best_so_far) > self.evaluate(self.locations[index]):
-            print("------: {} > {}".format(self.evaluate(self.best_so_far), self.evaluate(self.locations[index])))
+            if self.debug :
+                print("------: {} > {}".format(self.evaluate(self.best_so_far), self.evaluate(self.locations[index])))
+            self.best_so_far_iteration = self._current_iter
             self.best_so_far = self.locations[index]
-            print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
+            if self.debug :
+                print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
             
         
         worse = np.max(self.cost_matrix)
         index = int(np.where(self.cost_matrix == worse)[0])
         if self.worse_so_far is None :
             self.worse_so_far = self.locations[index]
-            print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
+            if self.debug :
+                print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
 
         if self.evaluate(self.worse_so_far) < self.evaluate(self.locations[index]):
-            print("------: {} < {}".format(self.evaluate(self.worse_so_far), self.evaluate(self.locations[index])))
+            if self.debug :
+                print("------: {} < {}".format(self.evaluate(self.worse_so_far), self.evaluate(self.locations[index])))
             self.worse_so_far = self.locations[index]
-            print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
+            if self.debug :
+                print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
 
         
             
@@ -178,10 +191,13 @@ class Algorithm:
             
             self._current_iter += 1
             
-            print("bsf: {} | cost: {}".format(self.best_so_far, self.evaluate(self.best_so_far )))
-            print("===="*20)
-            
-        quit()
+            if self.debug:
+                print("{} ---bsf: {} | cost: {}".format(self._current_iter -1 , self.best_so_far, self.evaluate(self.best_so_far )))
+                print("===="*20)
+        
+        print("\tBest individual seen fitness value: {:0.3f}".format(self.evaluate(self.best_so_far )))
+        print("\tBest individual seen generation apperared in: {}".format(self.best_so_far_iteration ))
+
         output_dictionary = {"iterations": [i for i in range(1, self.max_iter+1)], "avg": avg, "min": min_results, "max": max_results, "std": std}
         return self.best_so_far, self.evaluate(self.best_so_far), output_dictionary
 
