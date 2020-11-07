1d64fbd00e435f501f22b8ad28ed083d5cb9e207
Jamilgafur <jgafur@uiowa.edu>
2020-11-07 08:22:01 -0600

    made GSA more modular (same format as PSO)

--- GSA.py
+++ GSA.py
@@ -1,261 +1,175 @@
-import random
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import numpy as np
-from convex_quadratic_opt import generate_input as gi
-import matplotlib.tri as tri
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 14:58:39 2020
 
-np.set_printoptions(linewidth=np.inf)
+@author: jamilgafur
+"""
+from convex_quadratic_opt import generate_input as gi
+import numpy as np
+import statistics 
 
 def to_string():
     return "GSA"
-   
-class Algorithm:
-    
+
+class Particle:
+    def __init__(self, dimension, name):
+        self.name = name
+        self.position = []        # particle position
+        self.num_dimensions = dimension
+        self.force = [0 for i in range(self.num_dimensions)]
+        self.accel = [0 for i in range(self.num_dimensions)]
+        self.inertia = 0
+        self.velocity = [0 for i in range(self.num_dimensions)]
+        self.gravity = 0
+        self.mass = 0
+        self.cost = None
+        
+        for i in range(0,self.num_dimensions):
+            self.position.append(np.random.normal(0,1))
+        
+    # evaluate current fitness
+    def evaluate(self,costFunc):
+        self.cost = round(np.sum([i**2 for i in self.position.copy()]),2)#costFunc(self.position)
+        
+        
+    def calc_inertia(self, best, worst):
+        self.inertia = np.subtract(self.cost+ np.finfo('float32').eps, worst) / np.subtract(best, worst)
+        return self.inertia
+        
+    def calc_mass(self, total_inertia):
+        self.mass = np.divide(self.inertia, total_inertia)
+        
+    def update_gravity(self, current_iteration, total_iteration):
+        self.gravity = np.exp(-1 * (np.divide(current_iteration, total_iteration) ))
+
+    def calc_force(self,other_particle):
+        if not self.position == other_particle.position:
+            numerator = np.multiply(other_particle.mass, self.mass)
+            denominator = np.sqrt(np.subtract(self.position , other_particle.position) ** 2) + np.finfo('float32').eps
+            self.force += self.gravity * (numerator / denominator) * np.subtract(other_particle.position , self.position)
+            self.force *=  np.random.uniform(0,1)
+        
+        
+    def calc_acceleration(self):
+        self.accel = np.divide(self.force, self.mass)
+        
+    def calc_velocity(self):
+        self.velocity = np.random.uniform(0, 1) * np.add(self.velocity ,self.accel)
+        
+    # update the particle position based off new velocity updates
+    def update_position(self):
+        #name position cost inertia mass grav force accel velocity
+        tableformat = "{:5}{:10}{:10}\t{:10}\t{:10}\t{:10}\t{:10}\t{:10}\t{:10}".format(self.name,
+                                                        np.round(self.position[0],2),
+                                                        self.cost,
+                                                        np.round(self.inertia,2),
+                                                          np.round(self.mass,2),
+                                                          np.round(self.gravity,2),
+                                                        np.round( self.force[0],2),
+                                                        np.round( self.accel[0],2),
+                                                          np.round(self.velocity[0],2))
+        print(tableformat)
+        self.position =  np.add(self.position,self.velocity).tolist()
+                
+class Algorithm():
     def __init__(self, **args):
+        print("{:5}{:10}{:10}{:10}{:10}{:10}{:10}{:10}{:10}\n".format("name",
+                                                "position",
+                                                "cost",
+                                                "inertia",
+                                                "mass",
+                                                "gravity",
+                                                "force",
+                                                "accel",
+                                                "velocity"))
+    # ,costFunc,x0,bounds,num_particles,maxiter
         self.A = None
         self.b = None
-        self.k = args["k"]
         self.A, self.b = gi(args["k"], args["size"], args["debug"])
-        self.debug = args["debug"] 
-        self.G =  None
-        self.max_iter  = args["number_generations"]
-        self.pop_size  = args["pop_size"]
+
         self.dimension = args["size"]
+        self.best_cost = None                   # best error for group
+        self.best_cost_location = []                   # best position for group
+        self.best_iteration = 0
+        self.worse_cost = None                   # best error for group
+        self.worse_cost_location = []                   # best position for group
+        self.num_particles = args["pop_size"]
+        self.maxiter = args["number_generations"]
+        self.avg  = [] 
+        self.min_results = []
+        self.max_results = []
+        self.std = []
         
         if args["problems"] == 1:
-          self.cost_func = self.evalutate_quad_opt
+          self.costFunc = self.evalutate_quad_opt
         else:
-          self.cost_func = self.evalutate_quad_opt
-       
-        self._current_iter = 0
-        self.best_so_far_iteration = None
-        self.best_so_far = None
-        self.worse_so_far = None
-        
-        
-        self.locations = np.random.randint(low=-1, high=1,
-                size=(self.pop_size, self.dimension)) 
-        self.V = np.random.random((self.pop_size, self.dimension))
-        self.cost_matrix = np.full((self.pop_size, 1), None)
-        self.inertia = np.full((self.pop_size, 1), None)
-        self.accel = np.full((self.pop_size, self.dimension), None)
-        self.f = np.full((self.pop_size, self.dimension), None)
-        self.gen_cost_matrix()
+          self.costFunc = self.evalutate_quad_opt
+          
+        # establish the swarm
+        self.swarm=[]
+        for i in range(0,self.num_particles):
+            self.swarm.append(Particle(self.dimension, i))
 
-        if self.debug and len(self.A) == 2:
-            self.fig = plt.figure()
-            self.ax = plt.axes()
-            self.line, = self.ax.plot([], [], 'o', color='black')
-            self.history_loc = []
-            self.history_loc.append(self.locations)
-            
+    def run(self):
+        # begin optimization loop
+        for i in range(self.maxiter):
+            # cycle through particles in swarm and evaluate fitness
+            for particle in self.swarm:
+                particle.evaluate(self.costFunc)
+                # determine if current particle is the best (globally)
+                if self.best_cost == None or particle.cost < self.best_cost:
+                    self.best_cost = particle.cost
+                    self.best_cost_location = particle.position.copy()
+                    self.best_iteration = i
+                    
+                if self.worse_cost == None or particle.cost > self.worse_cost:
+                    self.worse_cost_location = particle.position.copy()
+                    self.worse_cost = particle.cost
+                    
+            total_intertia = 0
+            for particle in self.swarm:
+                total_intertia += particle.calc_inertia(self.best_cost, self.worse_cost)  
+                
+            for particle in self.swarm:
+                particle.calc_mass(total_intertia)
+                particle.update_gravity(i, self.maxiter)
+
+            for particle in self.swarm:
+                for other_particle in self.swarm:
+                    if particle.position != other_particle.position:
+                        particle.calc_force(other_particle)
+                particle.calc_acceleration()
+                particle.calc_velocity()
+                particle.update_position()
+                    
+                            
+            print("\t-----")
+            print("\tbest:{} \tworse:{}".format(self.best_cost,self.worse_cost))
+            print("\t-----")
+            smallest = min([particle.cost for particle in self.swarm ])
+            self.min_results.append(smallest)
+            largest = max([particle.cost for particle in self.swarm ])
+            self.max_results.append(largest)
+            self.avg.append(sum([particle.cost for particle in self.swarm])/self.num_particles)
+            self.std.append(statistics.stdev([particle.cost for particle in self.swarm ]))
+            print("----" * 10)
+
+
         
-        
+        # print("\tBest      cost: {}".format(self.best_cost))
+        # print("\tBest  location: {}".format(self.best_cost_location))
+        # print("\tBest iteration: {}".format(self.best_iteration))
+        quit()
+        output_dictionary = {"iterations": [i for i in range(1, self.maxiter+1)], "avg": self.avg, "min": self.min_results, "max": self.max_results, "std": self.std}
+        return self.pos_best_g, self.costFunc(self.pos_best_g), output_dictionary
+
+
     # optimization function 1
     def evalutate_quad_opt(self, individual):
         x = np.array(individual, dtype=float)
+        #value = 0.5 * np.matmul(np.matmul(x.T, self.A), x) - np.matmul(self.b.T, x)
         value = np.linalg.norm(np.matmul(self.A, x) - self.b, 2)
         return value
 
-    # Evaluate a single x (x_i)
-    def evaluate(self, args):
-        return self.cost_func(args) 
-
-    # Generate the cost of all particles
-    def gen_cost_matrix(self):
-            
-        for i, x in enumerate(self.locations):
-            self.cost_matrix[i] = self.evaluate(x)
-            
-        if self.debug > 1:
-            print("\tloc: \n{}".format(self.locations))
-            print("\tcmx: \n{}".format(self.cost_matrix))
-        
-        self.cal_inertia()
-
-    def cal_inertia(self):
-        best = np.min(self.cost_matrix)
-        worst = np.max(self.cost_matrix)
-        
-        self.inertia = (self.cost_matrix - worst) + np.finfo('float32').eps/ (best - worst)
-        
-        if self.debug > 1:
-            print("\tine: \n{}".format(self.inertia))
-        
-        self.cal_mass()
-            
-    def cal_mass(self):
-        self.mass_matrix = self.inertia / np.sum(self.inertia)
-        
-        if self.debug > 1:
-            print("\tmas: \n{}".format(self.mass_matrix))
-        
-        self.update_grav()
-
-
-    def update_grav(self):
-        self.G = 9.81 * np.exp(-1 * (self._current_iter / self.max_iter) )
-        self.cal_force()
-        
-    def cal_force(self):
-        costs = self.cost_matrix.copy()
-#        costs.sort(axis=0)
-        
-        for i in range(self.pop_size):
-            f = None
-            for j in range(costs.size):
-                dividend = float(self.mass_matrix[i] * self.mass_matrix[j])
-                divisor = np.sqrt(np.sum((self.locations[i] - self.locations[j]) ** 2)) + np.finfo('float32').eps
-                if f is None:
-                    f = self.G * (dividend / divisor) * (self.locations[j] - self.locations[i])
-                else:
-                    f = f + self.G * (dividend / divisor) * (self.locations[j] - self.locations[i])
-            f = f * random.random()
-            self.f[i] = f 
-            
-        if self.debug > 1:
-            print("\tfce: \n{}".format(self.f))
-        
-        self.cal_accel()
-
-    def cal_accel(self):
-        self.accel = self.f / self.mass_matrix
-        
-        self.cal_vel()
-
-    def cal_vel(self):
-        self.V = (np.random.uniform(0, 1) * self.V) + self.accel
-
-        if self.debug > 1:
-            print("\tvel: \n{}".format(self.V))
-        
-        self.update_locations()
-        
-        
-    def update_locations(self):
-        self.locations = self.locations + self.V
-
-        if self.debug > 1:
-            print("\tnlc: \n{}".format(self.locations))
-
-        self.update_best_so_far()
-
-    # set for minimization
-    def update_best_so_far(self):
-        best = np.min(self.cost_matrix)
-        index = np.where(self.cost_matrix == best)[0][0]
- 
-        if self.best_so_far is None:
-            self.best_so_far = self.locations[index]
-            self.best_so_far_iteration = self._current_iter
-        
-            if self.debug > 1:
-                print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
-        
-        if self.evaluate(self.best_so_far) > self.evaluate(self.locations[index]):
-            self.best_so_far_iteration = self._current_iter
-            self.best_so_far = self.locations[index]
-            
-            if self.debug > 1:
-                print("------: {} > {}".format(self.evaluate(self.best_so_far), self.evaluate(self.locations[index])))
-                print("new best: {} @ {}".format(self.best_so_far, self.evaluate(self.best_so_far)))
-            
-            
-        worse = np.max(self.cost_matrix)
-        index = np.where(self.cost_matrix == worse)[0][0]
-        if self.worse_so_far is None :
-            self.worse_so_far = self.locations[index]
-            
-            if self.debug > 1:
-                print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
-         
-        if self.evaluate(self.worse_so_far) < self.evaluate(self.locations[index]):
-            
-            if self.debug > 1:
-                print("------: {} < {}".format(self.evaluate(self.worse_so_far), self.evaluate(self.locations[index])))
-            
-            self.worse_so_far = self.locations[index]
-            
-            if self.debug > 1:
-                print("new worse: {} @ {}".format(self.worse_so_far, self.evaluate(self.worse_so_far)))
-
-
-    def init_animation(self):
-        self.line.set_data([],[])
-        return self.line, 
-
-    def animate(self, time):
-        x = []
-        y =[]
-        for i in self.history_loc[time]:
-            x.append(i[0])
-            y.append(i[1])
-        
-        self.line.set_data(x,y)
-        
-        return self.line, 
-           
-    def run(self):
-        
-        avg = []
-        min_results = [] 
-        max_results = []
-        avg = []
-        std = []        
-        while self._current_iter <= self.max_iter:
-            print("{} / {}".format(self._current_iter,self.max_iter))
-            self.gen_cost_matrix()
-            if self.debug > 2 and len(self.A) == 2:
-                self.history_loc.append(self.locations)
-            
-        
-            itermin = np.min(self.cost_matrix)
-            itermax = np.max(self.cost_matrix)
-    
-            min_results.append(itermin)
-            max_results.append(itermax)
-            std.append(np.std(itermin))
-            avg.append((itermin+itermax )/ 2)
-
-            self._current_iter += 1
-            
-            if self.debug > 1:
-                print("{} ---bsf: {} | cost: {}".format(self._current_iter -1 , self.best_so_far, self.evaluate(self.best_so_far)))
-                print("===="*20)
-                if (len(self.A) == 2):
-                    self.history_loc.append(self.locations)
-                    self.history_V.append(self.V)
-                    self.ax.axis([-20,20,-20,20])
-                    anim = FuncAnimation(self.fig, self.animate, init_func=self.init_animation, frames=self._current_iter+2, interval=500, blit=True)
-                    
-                    npts = 10000
-                    x = np.random.uniform(-20, 20, npts)
-                    y = np.random.uniform(-20, 20, npts)
-                    z = []
-                    for r, c in zip(x, y):
-                        z.append(self.evaluate([r, c]).item(0))
-                       
-                    triang = tri.Triangulation(x, y)
-                    interpolator = tri.LinearTriInterpolator(triang, z)
-                    
-                    ngridx = 10000
-                    ngridy = 10000
-                    xi = np.linspace(-20.1, 20.1, ngridx)
-                    yi = np.linspace(-20.1, 20.1, ngridy)
-                    
-                    Xi, Yi = np.meshgrid(xi, yi)
-                    zi = interpolator(Xi, Yi)
-                    
-                    plt.contourf(Xi, Yi, zi, 20, cmap='RdGy')
-                    plt.colorbar()
-                    anim.save('animate.gif', writer='imagemagick')
-                    
-        print("\tGSA Best individual seen fitness value: {}".format(self.evaluate(self.best_so_far )))
-        print("\tGSA Best individual seen location: {}".format(self.best_so_far ))
-        print("\tGSA Best individual seen generation apperared in: {}".format(self.best_so_far_iteration ))
-        
-        #print("\n\nExact minimizer for problem is: %r" % (np.asarray(np.matmul(np.linalg.inv(self.A), self.b))))
-        output_dictionary = {"iterations": [i for i in range(self.max_iter)], "avg": avg, "min": min_results, "max": max_results, "std": std}
-        return self.best_so_far, self.evaluate(self.best_so_far), output_dictionary
 
